c420233d8d4896e178e22df0c855f00fa05c5172
wzp <1204473090@qq.com>
2018-07-30 16:32:09 +0800

    基础库类

--- Example/Pods/Target Support Files/BaseModule/BaseModule-umbrella.h
+++ Example/Pods/Target Support Files/BaseModule/BaseModule-umbrella.h
@@ -10,6 +10,17 @@
 #endif
 #endif
 
+#import "MKMapView+Annotation.h"
+#import "NSData+CTHash.h"
+#import "NSDate+CTString.h"
+#import "NSObject+CTAlert.h"
+#import "NSObject+CTBundle.h"
+#import "NSObject+CTIP.h"
+#import "NSObject+CTNavigation.h"
+#import "NSString+CTDate.h"
+#import "NSString+CTHash.h"
+#import "NSString+CTHeight.h"
+#import "NSString+CTURL.h"
 
 FOUNDATION_EXPORT double BaseModuleVersionNumber;
 FOUNDATION_EXPORT const unsigned char BaseModuleVersionString[];
